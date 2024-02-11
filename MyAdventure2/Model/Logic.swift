a108366fa90add8a5fbe3c2bd6f926b143232fa4
aws91031 <aws91031@gmail.com>
2024-02-11 01:39:31 -0500

    Refactored app using MVC pattern

---
+++ MyAdventure2/Model/Logic.swift
@@ -0,0 +1,8 @@
+//
+//  Logic.swift
+//  MyAdventure2
+//
+//  Created by user250993 on 2/11/24.
+//
+
+import Foundation
---
+++ MyAdventure2/Model/Question.swift
@@ -0,0 +1,8 @@
+//
+//  Question.swift
+//  MyAdventure2
+//
+//  Created by user250993 on 2/11/24.
+//
+
+import Foundation
